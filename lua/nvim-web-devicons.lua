4f5543dee40b0fb23f38fac9c63fc4a43f151452
Luca Hagel <luca@luhagel.com>
2022-09-23 17:34:37 +0200

    Icons: Add graphql support

--- lua/nvim-web-devicons.lua
+++ lua/nvim-web-devicons.lua
@@ -596,6 +596,12 @@ local icons = {
     cterm_color = "66",
     name = "GodotProject",
   },
+  ["graphql"] = {
+    icon = "",
+    color = "#e535ab",
+    cterm_color = "199",
+    name = "GraphQL"
+  },
   ["gruntfile"] = {
     icon = "",
     color = "#e37933",
@@ -1433,6 +1439,7 @@ local filetypes = {
   ["glb"] = "glb",
   ["go"] = "go",
   ["godot"] = "godot",
+  ["graphql"] = "graphql",
   ["gruntfile"] = "gruntfile",
   ["gulpfile"] = "gulpfile",
   ["haml"] = "haml",
