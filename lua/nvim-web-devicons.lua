dd468f6991a4e447607097dfc89d47ae97d14dea
nobuhikosawai <nobuhiko.sawai@gmail.com>
2023-03-06 12:45:54 +0900

    fix: handle extension with multiple dots (#216)
    * fix: handle extension with multiple dots
    
    * fix: extract recursive function

--- lua/nvim-web-devicons.lua
+++ lua/nvim-web-devicons.lua
@@ -1868,10 +1868,35 @@ local function get_default_icon()
   return default_icon
 end
 
+-- recursively iterate over each segment separated by '.' to parse extension with multiple dots in filename
+local function iterate_multi_dotted_extension(name, icon_table)
+  if name == nil then
+    return nil
+  end
+
+  local compound_ext = name:match("%.(.*)")
+  local icon = icon_table[compound_ext]
+  if icon then
+    return icon
+  end
+
+  return iterate_multi_dotted_extension(compound_ext, icon_table)
+end
+
+local function get_icon_by_extension(name, ext, opts)
+  local is_strict = if_nil(opts and opts.strict, global_opts.strict)
+  local icon_table = is_strict and icons_by_file_extension or icons
+
+  if ext ~= nil then
+    return icon_table[ext]
+  end
+
+  return iterate_multi_dotted_extension(name, icon_table)
+end
+
 local function get_icon(name, ext, opts)
 	name = name:lower()
 
-  ext = ext or name:match("^.*%.(.*)$") or ""
   if not loaded then
     setup()
   end
@@ -1880,9 +1905,9 @@ local function get_icon(name, ext, opts)
   local is_strict = if_nil(opts and opts.strict, global_opts.strict)
   local icon_data
   if is_strict then
-    icon_data = icons_by_filename[name] or icons_by_file_extension[ext] or (has_default and default_icon)
+    icon_data = icons_by_filename[name] or get_icon_by_extension(name, ext, opts) or (has_default and default_icon)
   else
-    icon_data = icons[name] or icons[ext] or (has_default and default_icon)
+    icon_data = icons[name] or get_icon_by_extension(name, ext, opts) or (has_default and default_icon)
   end
 
   if icon_data then
@@ -1902,7 +1927,6 @@ local function get_icon_by_filetype(ft, opts)
 end
 
 local function get_icon_colors(name, ext, opts)
-  ext = ext or name:match("^.*%.(.*)$") or ""
   if not loaded then
     setup()
   end
@@ -1911,9 +1935,9 @@ local function get_icon_colors(name, ext, opts)
   local is_strict = if_nil(opts and opts.strict, global_opts.strict)
   local icon_data
   if is_strict then
-    icon_data = icons_by_filename[name] or icons_by_file_extension[ext] or (has_default and default_icon)
+    icon_data = icons_by_filename[name] or get_icon_by_extension(name, ext, opts) or (has_default and default_icon)
   else
-    icon_data = icons[name] or icons[ext] or (has_default and default_icon)
+    icon_data = icons[name] or get_icon_by_extension(name, ext, opts) or (has_default and default_icon)
   end
 
   if icon_data then
