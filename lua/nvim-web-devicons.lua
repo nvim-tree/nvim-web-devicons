7329cabaa5b85a8d7fdfc84042947b36ce219359
gegoune <dev@clog.rocks>
2023-04-11 20:40:08 +0300

    revert: "feat(icon): add testy for testing"
    This reverts commit 627164d1243ac770583436be16f4fa9ebc3eae43.

--- lua/nvim-web-devicons.lua
+++ lua/nvim-web-devicons.lua
@@ -1,11 +1,5 @@
 -- exact match by file name
 local icons_by_filename = {
-  [".testy"] = {
-    icon = "ﬥ",
-    color = "#cbcb41",
-    cterm_color = "185",
-    name = "Babelrc",
-  },
   [".babelrc"] = {
     icon = "ﬥ",
     color = "#cbcb41",
