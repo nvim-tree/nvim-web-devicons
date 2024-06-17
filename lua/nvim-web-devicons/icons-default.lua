b62d8ba070886f9a7c651a9e4af94d5eda7c5674
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-06-17 11:03:49 -0700

    feat: add gradle file extension

--- lua/nvim-web-devicons/icons-default.lua
+++ lua/nvim-web-devicons/icons-default.lua
@@ -1610,6 +1610,12 @@ local icons_by_file_extension = {
     cterm_color = "199",
     name = "GraphQL",
   },
+  ["gradle"] = {
+    icon = "",
+    color = "#005f87",
+    cterm_color = "24",
+    name = "Gradle",
+  },
   ["graphql"] = {
     icon = "",
     color = "#e535ab",
