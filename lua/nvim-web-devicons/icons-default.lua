e73d2774d12d0ecf9e05578d692ba1ea50508cf2
Charles Dong <chardon_cs@proton.me>
2024-12-10 22:40:49 +0800

    fix: update D lang color (#520)

--- lua/nvim-web-devicons/icons-default.lua
+++ lua/nvim-web-devicons/icons-default.lua
@@ -1534,8 +1534,8 @@ local icons_by_file_extension = {
   },
   ["d"] = {
     icon = "",
-    color = "#427819",
-    cterm_color = "28",
+    color = "#b03931",
+    cterm_color = "124",
     name = "D",
   },
   ["d.ts"] = {
