37334adf4517fecfd97c0b44e1d4718e377e9e52
gegoune <69750637+gegoune@users.noreply.github.com>
2025-01-25 10:36:23 +0100

    style: normalised and formatted (single icon per line) icon tables, light icons completely generated (#543)
    Co-authored-by: hasecilu <hasecilu@tuta.io>

--- Makefile
+++ Makefile
@@ -1,21 +1,31 @@
 VIM_COLORTEMPLATE_VERSION = 2.2.3
+VIM_MINI_ALIGN_VERSION = 0.14.0
 
 all: colors style-check lint filetypes
 
-colors: vim-colortemplate
+colors: vim-colortemplate mini-align
+	rm lua/nvim-web-devicons/icons-light.lua
+	cp lua/nvim-web-devicons/icons-default.lua lua/nvim-web-devicons/icons-light.lua
 	nvim \
 		--clean \
 		--headless \
 		--cmd "set rtp^=vim-colortemplate" \
+		--cmd "set rtp^=mini-align" \
 		-c 'source scripts/generate_colors.lua' \
+		-c 'source scripts/align_spaces.lua' \
 		-c 'qall'
 
 colors-check: colors
+	git diff --exit-code lua/nvim-web-devicons/icons-default.lua
 	git diff --exit-code lua/nvim-web-devicons/icons-light.lua
 
 vim-colortemplate:
 	mkdir -p vim-colortemplate
 	curl -L https://github.com/lifepillar/vim-colortemplate/archive/refs/tags/v$(VIM_COLORTEMPLATE_VERSION).tar.gz | tar zx --directory vim-colortemplate --strip-components=1
+
+mini-align:
+	mkdir -p mini-align
+	curl -L https://github.com/echasnovski/mini.align/archive/refs/tags/v$(VIM_MINI_ALIGN_VERSION).tar.gz | tar zx --directory mini-align --strip-components=1
 
 style-check:
 	stylua . --check
@@ -31,5 +41,6 @@ filetypes:
 
 clean:
 	rm -rf vim-colortemplate
+	rm -rf mini-align
 
 .PHONY: all colors style-check style-fix lint filetypes
--- scripts/filetypes.sh
+++ scripts/filetypes.sh
@@ -3,31 +3,20 @@
 # Iterate over the elements of icons_by_file_extension and check if there are missed filetypes.
 # $VIMRUNTIME specifies neovim runtime path, defaults to "/usr/share/nvim/runtime" if unset.
 
-if [ -z "${VIMRUNTIME}" ]; then
-	export VIMRUNTIME="/usr/share/nvim/runtime"
-fi
+: "${VIMRUNTIME:=/usr/share/nvim/runtime}"
 
 exit_value=0
-in_section=false
 
-while IFS= read -r line; do
-	if [[ $line =~ ^local\ icons_by_file_extension\ =\ \{$ ]]; then
-		in_section=true
-	elif [[ $line =~ ^\}$ ]]; then
-		in_section=false
-	fi
+while read -r key; do
+  # Search for the key in the main file
+  line=$(grep -F "\"$key\"" "lua/nvim-web-devicons.lua")
+  if [ -z "$line" ]; then
+    [ -f "${VIMRUNTIME}/syntax/$key.vim" ] &&
+      echo -e "\e[33mPlease add \"$key\" to filetypes Lua table.\e[0m" &&
+      exit_value=1
+  fi
+done < <(
+  sed -nr '/^local icons_by_file_extension/,/^}/!d; s/.*\["(.*)"\].*/\1/p' lua/nvim-web-devicons/icons-default.lua
+)
 
-	if $in_section && [[ $line =~ \[\"(.+)\"\] ]]; then
-		key="${BASH_REMATCH[1]}"
-		# Search for the key in the main file
-		line=$(grep -F "\"$key\"" "lua/nvim-web-devicons.lua")
-		if [ -n "$line" ]; then
-			continue
-		else
-			[ -f "${VIMRUNTIME}/syntax/$key.vim" ] &&
-				echo -e "\e[33mPlease add \"$key\" to filetypes Lua table.\e[0m" &&
-				exit_value=1
-		fi
-	fi
-done <"lua/nvim-web-devicons/icons-default.lua"
 exit $exit_value
